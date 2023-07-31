5994420c6fea6a339867681757b4028a556dcfab
ggimenez01 <126508424+ggimenez01@users.noreply.github.com>
2023-07-31 11:05:29 -0500

    Update active_admin.rb initializers

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -14,7 +14,7 @@ ActiveAdmin.setup do |config|
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
   #
-  # Note: Aim for an image that's 21px high so it fits in the header.
+  # Note: Aim for an image that's 21px high so it fits in the header
   #
   # config.site_title_image = "logo.png"
 
